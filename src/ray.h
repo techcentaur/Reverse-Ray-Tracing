e7a390e5ac106aaa91d243a33c3f912c4e728e64
Ankit Solanki <ankit03june@gmail.com>
2019-01-22 22:15:17 +0530

    ADD ray operation working fine (Have a direction and origin)

--- src/ray.h
+++ src/ray.h
@@ -2,16 +2,21 @@
 #define RAY_H
 
 #include "vector.h"
+using namespace std;
 
-class Ray{
+class Ray3f{
 
 public:
-	Vector origin, direction;
 
-	Ray();
-	Ray(Vector &vec1, Vector &vec2);
-	
-	void print();
+	Vector3f origin;
+	Vector3f direction;
+
+	Ray3f() {}
+	Ray3f(Vector3f &v1, Vector3f &v2) {origin = v1; direction = v2;}
+
+	// make a function to take t, and return Vector
+	void print(){origin.print(); direction.print();}
+
 };
 
 #endif
