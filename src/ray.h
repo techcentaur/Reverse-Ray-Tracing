b7f475a4e627ce95e0ce92d1f5849c22e630ce10
Ankit Solanki <ankit03june@gmail.com>
2019-01-27 16:18:28 +0530

    Definition of ray changed from direction being a point to a direction itself

--- src/ray.h
+++ src/ray.h
@@ -12,11 +12,13 @@ public:
 	Vector3f direction;
 
 	Ray3f() {}
-	Ray3f(Vector3f &v1, Vector3f &v2) {origin = v1; direction = v2;}
+	Ray3f(Vector3f &v1, Vector3f &v2) {origin = v1; direction = (v2 - v1).normalizeIt();}
+	void Ray3fWithDirection(Vector3f &v, Vector3f &direc){ origin = v; direction = direc;}
 
 	// make a function to take t, and return Vector
-	void print(){cout<<"[*] Ray: "; origin.print(); direction.print();}
-
+	void print(){cout<<"[*] Ray: \n";
+				cout<<"Origin: "; origin.print();
+				cout<<"Direction: "; direction.print();}
 };
 
 #endif
