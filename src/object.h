039531bcd9b45d47809139889c1087cb8d923809
Khichi-Hardik <hardikkhichi@gmail.com>
2019-01-31 02:25:15 +0530

    object.cpp updated for plane

--- src/object.h
+++ src/object.h
@@ -46,19 +46,18 @@ public:
 	vector<float> bounds;
 	
 	Plane();
-	Plane(const Plane &p);
 	Plane(Vector3f &p, Vector3f &n, Material &m, vector<float> b);
 	Plane(Vector3f &p1, Vector3f &p2, Vector3f &p3, Material &m, vector<float> b);
 
 	bool getIntersection(Ray3f &ray, float &param);
-	tuple<float, Vector3f> distanceAlongRay(Ray3f &ray);
+	Vector3f getNormalOnIntersectionPoint(Vector3f &point, Ray3f &ray);
+	void print();
 
 	void translation(const Vector3f &transform);
 	void rotateAboutX(float angle);
 	void rotateAboutY(float angle);
 	void rotateAboutZ(float angle);
 
-	void print();
 };
 
 #endif
