4b90f8b1971e5f86a82c894a9b2950e5d0dac55c
Khichi-Hardik <hardikkhichi@gmail.com>
2019-02-16 21:02:35 +0530

    Last commit before OpenGL switch

--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,2 @@
-g++ -o test main.cpp color.cpp tracer.cpp vector.cpp light.cpp object.cpp -std=c++11
+g++ -o test main.cpp color.cpp tracer.cpp vector.cpp light.cpp object.cpp transformation.cpp matrix.cpp -std=c++11
 ./test
