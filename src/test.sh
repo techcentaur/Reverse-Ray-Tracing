e86d84b615f9596197192e85a88efb146fbdd9d4
Khichi-Hardik <hardikkhichi@gmail.com>
2019-02-18 23:45:20 +0530

    window working for glfw

--- src/test.sh
+++ src/test.sh
@@ -1,3 +1,3 @@
 rm test
-g++ test.cpp -o test -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lGLEW -lsfml-system -lsfml-window -lsfml-graphics
+g++ test.cpp -o test -lglfw -lGLU -lGL -lGLEW -lX11 -lXxf86vm -lXrandr -lpthread -lXi -lXinerama -lXcursor -lXi -ldl -lsfml-system -lsfml-window -lsfml-graphics
 ./test
